387ca67497e77fa713e7e541003113151bab01eb
dreamcatcher696 <thomaseckert@MacBook-Pro-van-Thomas.local>
2017-01-02 14:20:00 +0100

    added validation layout

--- resources/views/fileupload.blade.php
+++ resources/views/fileupload.blade.php
@@ -42,22 +42,31 @@
 
     <form method="POST" action="files/upload" enctype="multipart/form-data" class="form-horizontal">
       {{ csrf_field() }}
-      <div class="form-group">
+      <div class="form-group {{$errors->has('titel') ? 'has-error has-feedback' : ''}}">
         <label class="control-label col-sm-2" for="titel">Titel</label>
         <div class="col-sm-10">
-          <input class="form-control" type="text" name="titel" placeholder="voeg een titel toe" value="{{old('titel')=='' ? '' : old('titel')}}"></input>
+          <input class="form-control" type="text" name="titel" placeholder="voeg een titel toe" value="{{old('titel')=='' ? '' : old('titel')}}">
+            @if($errors->has('titel'))
+              <span class="glyphicon glyphicon-remove form-control-feedback"></span>
+            @endif
         </div>
       </div>
-      <div class="form-group">
+      <div class="form-group {{$errors->has('aankoop_datum') ? 'has-error has-feedback' : ''}}">
         <label class="control-label col-sm-2" for="aankoop_datum">Aankoopdatum</label>
         <div class="col-sm-10">
-          <input class="form-control" type="date" name="aankoop_datum" value="{{old('aankoop_datum')=='' ? '' : old('aankoop_datum')}}"></input>
+          <input class="form-control" type="date" name="aankoop_datum" value="{{old('aankoop_datum')=='' ? '' : old('aankoop_datum')}}">
+          @if($errors->has('aankoop_datum'))
+              <span class="glyphicon glyphicon-remove form-control-feedback"></span>
+            @endif
         </div>
       </div>
-      <div class="form-group">
+      <div class="form-group {{$errors->has('verloop_datum') ? 'has-error has-feedback' : ''}}">
         <label class="control-label col-sm-2" for="verloop_datum">Verloopt op</label>
         <div class="col-sm-10">
-          <input class="form-control" type="date" name="verloop_datum" value="{{old('verloop_datum')=='' ? '' : old('verloop_datum')}}"></input>
+          <input class="form-control" type="date" name="verloop_datum" value="{{old('verloop_datum')=='' ? '' : old('verloop_datum')}}">
+          @if($errors->has('verloop_datum'))
+              <span class="glyphicon glyphicon-remove form-control-feedback"></span>
+            @endif
         </div>
       </div>
       <div class="form-group">
@@ -66,10 +75,11 @@
           <textarea class="form-control" type="text" name="beschrijving" placeholder="Voeg een korte beschrijving in">{{old('beschrijving')}}</textarea>
         </div>
       </div>
-      <div class="form-group">
+      <div class="form-group {{$errors->has('filename') ? 'has-error has-feedback' : ''}}">
         <label class="control-label col-sm-2" for="file">Bestand</label>
         <div class="col-sm-10">
-          <input type="file" accept="application/pdf" name="filename" id="file" data-allowed-file-extensions='["pdf"]'></input>
+          <input type="file" accept="application/pdf" name="filename" id="file" data-allowed-file-extensions='["pdf"]'>
+          
         </div>
       </div>
       <div class="col-sm-2"></div>
